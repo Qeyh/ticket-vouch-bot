b5a5831ac4f1636e997bc5c767a4a64d0b6ee597
Qeyh <daryakerman2002@gmail.com>
2020-08-23 02:41:35 +0300

    added setup command

--- index.js
+++ index.js
@@ -7,9 +7,31 @@ client.once('ready', () => {
 });
 
 client.on('message', msg => {
+
+  let ticketID = 0;
+
+  if(msg.content === `${prefix}setup`) {
+    let role = msg.guild.roles.cache.find(r => r.name === "Tickets");
+    if(!role && msg.member.hasPermission('ADMINISTRATOR')) { 
+      msg.reply('Setting up the role.');
+      msg.guild.roles.create({
+        data: {
+          name: 'Tickets',
+          color: 'BLUE',
+        },
+        reason: 'The role is needed for the bot.'
+      }).then(console.log("The role was created.")).catch(console.error);
+    }else if(!msg.member.hasPermission('ADMINISTRATOR')){
+      msg.reply(`You don't have premissions to setup the bot!`);
+    } else{
+      msg.reply(`The bot is already setup`);
+    } 
+  }
+
   if (msg.content === `${prefix}head`) {
     msg.channel.send('ass!');
   }
 });
+
 
 client.login(token);
