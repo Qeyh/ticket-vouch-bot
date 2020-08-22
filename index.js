af54833784a88c4cee54e0ac67d6b4226486b52e
Qeyh <daryakerman2002@gmail.com>
2020-08-23 01:08:46 +0300

    added token and changed if command

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {token} = require('./config.json');
+const {token, prefix} = require('./config.json');
 
 client.once('ready', () => {
     console.log("The bot is online, Pog!");
 });
 
 client.on('message', msg => {
-    if (msg.content === '-head') {
-      msg.channel.send('ass!');
-    }
-  });
+  if (msg.content === `${prefix}head`) {
+    msg.channel.send('ass!');
+  }
+});
 
 client.login(token);
