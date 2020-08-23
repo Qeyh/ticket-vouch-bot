a1881ff1d5966b5b823ea75ed70a9239191bd844
Qeyh <daryakerman2002@gmail.com>
2020-08-23 03:13:01 +0300

    Upgraded setup command.

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ client.once('ready', () => {
 });
 
 client.on('message', msg => {
-
+  let role = msg.guild.roles.cache.find(r => r.name === "Tickets");
+  let ticketsCategory = msg.guild.channels.cache.find(r => r.name === "open tickets");
   let ticketID = 0;
 
-  if(msg.content === `${prefix}setup`) {
-    let role = msg.guild.roles.cache.find(r => r.name === "Tickets");
-    if(!role && msg.member.hasPermission('MANAGE_CHANNELS')) { 
-      msg.reply('Setting up the role.');
+  if(msg.content === `${prefix}setup`) { 
+    if(!role && !ticketsCategory && msg.member.hasPermission('MANAGE_CHANNELS')) { 
+      msg.reply('Setting up the bot.');
+      //Create a new role with the name "Tickets".
       msg.guild.roles.create({
         data: {
           name: 'Tickets',
@@ -21,12 +22,21 @@ client.on('message', msg => {
         },
         reason: 'The role is needed for the bot.'
       }).then(console.log("The role was created.")).catch(console.error);
+      //Create a new category with the name "open tickets"
+      msg.guild.channels.create("open tickets", {
+        type: 'category',
+        reason: 'We need a category for open tickets.'
+      }).then(console.log("The category has been created.")).catch(console.error);
     }else if(!msg.member.hasPermission('MANAGE_CHANNELS')){
       msg.reply(`You don't have premissions to setup the bot!`);
-    } else{
-      msg.reply(`The bot is already setup`);
-    } 
+    } else if(ticketsCategory && role){
+      msg.reply(`The bot is already setup.`);
+    } else {
+      msg.reply(`Please make sure the role "Tickets" and the category "Open-tickets" were created. If not, delete the Role/Category and try again.`);
+    }
   }
+
+
 
   if (msg.content === `${prefix}head`) {
     msg.channel.send('ass!');
